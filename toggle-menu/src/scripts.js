4f8421b5f3c54febe1123cdb383b461d54aef4cd
fcastrogarcia <fede.castrog@gmail.com>
2019-06-17 17:15:13 -0300

    ready

--- toggle-menu/src/scripts.js
+++ toggle-menu/src/scripts.js
@@ -1,8 +1,20 @@
-const menuButton = document.querySelector(".toggle-container");
+const menu_button = document.querySelector(".toggle-container");
 const bars = document.querySelectorAll(".bar");
+const menu = document.querySelector(".menu");
+const menu_options = document.querySelectorAll(".modal span");
+const sections = document.querySelectorAll(".section");
+let selected = "1";
 
 function toggleMenu() {
   bars.forEach(bar => bar.classList.toggle("toggle"));
+  menu.classList.toggle("toggle");
 }
-
-menuButton.addEventListener("click", toggleMenu);
+function selectOption(e) {
+  sections.forEach(section => section.classList.remove("selected-option"));
+  selected = e.target.dataset.option;
+  sections[selected - 1].classList.add("selected-option");
+  toggleMenu();
+}
+//listeners
+menu_button.addEventListener("click", toggleMenu);
+menu_options.forEach(button => button.addEventListener("click", selectOption));
